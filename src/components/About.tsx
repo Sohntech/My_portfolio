021c4095f70e5e27215f903dfd8fff720568cc9d
Sohntech <ndiagalo259@gmail.com>
2024-11-24 10:12:28 +0000

    pdf en ligne

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -41,7 +41,7 @@ export const About: React.FC = () => {
           </motion.p>
 
           <motion.a
-            href="https://pdf.ac/1h1ulj" 
+            href="https://res.cloudinary.com/drxouwbms/image/upload/v1732391039/ychxwc4zfwqixahlpuct.pdf" 
             download
             className="inline-flex items-center px-6 py-3 rounded-lg bg-gradient-to-r from-[var(--neon-primary)] to-[var(--neon-secondary)] text-black dark:text-white font-medium space-x-2 hover:opacity-90 transition-opacity"
             whileHover={{ scale: 1.05 }}
